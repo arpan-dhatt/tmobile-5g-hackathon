6d145a74165e226ea9d54ed99b6c63bb7f1b3e2a
Arpan Dhatt <ary.dhatt@gmail.com>
2021-05-13 15:21:24 -0500

    Initial Commit

---
+++ tmobile-5g-hackathon/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  tmobile-5g-hackathon
+//
+//  Created by Arpan Dhatt on 5/13/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ tmobile-5g-hackathon/tmobile_5g_hackathonApp.swift
@@ -0,0 +1,17 @@
+//
+//  tmobile_5g_hackathonApp.swift
+//  tmobile-5g-hackathon
+//
+//  Created by Arpan Dhatt on 5/13/21.
+//
+
+import SwiftUI
+
+@main
+struct tmobile_5g_hackathonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
