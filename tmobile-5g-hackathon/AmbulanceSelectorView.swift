d9b6f03986a1b6c5d5d35e45e5da4789ec33acc7
Saaketh C <Saakethrchenna@gmail.com>
2021-05-14 15:35:03 -0500

    added the ambulance sector page

--- tmobile-5g-hackathon/AmbulanceSelectorView.swift
+++ tmobile-5g-hackathon/AmbulanceSelectorView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct AmbulanceSelectorView: View {
     @ObservedObject var ambulanceData = AmbulanceListDataSource()
+    @EnvironmentObject var viewModel: MainViewModel
     @State var text: String = ""
     
     var body: some View {
@@ -16,25 +17,34 @@ struct AmbulanceSelectorView: View {
             
             VStack{
                 Text("Select Ambulance").font(.largeTitle).bold().padding(.vertical,50)
-                Divider()
+                Divider().background(Color.black).padding(.bottom)
                 
                 ScrollView(.vertical, showsIndicators: false){
-                    HStack{
+                    HStack(alignment: .top) {
                         VStack{
                             ForEach(Array(zip(ambulanceData.items.indices, ambulanceData.items)), id: \.0){ (index, item) in
                                 if(index%2 == 0){
-                                    AmbulanceInfoCardView(name: ambulanceData.items[index].name, status: item.going_to, estimatedArrival: item.arriving_in, availibility: item.going_to)
+                                    AmbulanceInfoCardView(name: ambulanceData.items[index].name, status: item.going_to, estimatedArrival: item.arriving_in, availibility: item.going_to, ambulance: ambulanceData.items[index])
                                 }
                             }
                         }
                         VStack{
                             ForEach(Array(zip(ambulanceData.items.indices, ambulanceData.items)), id: \.0){ (index, item) in
                                 if(index%2 == 1){
-                                    AmbulanceInfoCardView(name: ambulanceData.items[index].name, status: item.going_to, estimatedArrival: item.arriving_in, availibility: item.going_to)
+                                    AmbulanceInfoCardView(name: ambulanceData.items[index].name, status: item.going_to, estimatedArrival: item.arriving_in, availibility: item.going_to, ambulance: ambulanceData.items[index])
                                 }
                             }
                         }
                     }
+                }
+                if viewModel.currentAmbulance.name != "none"{
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                    HStack{
+                        
+                        Text("Get Started").font(.title).foregroundColor(.white)
+                        
+                    }.padding().padding(.horizontal).background(Color.green).cornerRadius(10.0).padding(.bottom).padding()
+                })
                 }
             }.padding().background(Color.pink).cornerRadius(25.0).padding(50)
         }.onAppear{
@@ -44,21 +54,15 @@ struct AmbulanceSelectorView: View {
 }
 
 struct AmbulanceInfoCardView: View {
+    @EnvironmentObject var viewModel: MainViewModel
     var name: String
     var status: String
     var estimatedArrival: String
     var availibility: String
-    var selection = { (selection: String) -> String in
-        return selection
-    }
-    var selected = { (selected: DarwinBoolean) -> Color in
-        if selected == true {
-            return Color.green
-        }
-        return Color.white
-    }
+    var ambulance: Ambulance
     
     var body: some View {
+        if viewModel.currentAmbulance.name == name{
         VStack(alignment: .leading){
             HStack{
                 Text("Ambulance: "+name).font(.headline)
@@ -89,12 +93,47 @@ struct AmbulanceInfoCardView: View {
             
             Text("En Route To: "+status).font(.headline).padding(.top, 10)
             Text("Arriving In: "+estimatedArrival).font(.headline)
-        }.padding().padding(.horizontal).cornerRadius(10.0).frame(maxHeight: 250).overlay(RoundedRectangle(cornerRadius: 10.0).stroke(Color.black, lineWidth: 1)).padding()
+        }.padding().padding(.horizontal).cornerRadius(10.0).frame(maxHeight: 300).overlay(RoundedRectangle(cornerRadius: 10.0).stroke(Color.green, lineWidth: 3)).padding()
+        }
+        else {
+            VStack(alignment: .leading){
+                HStack{
+                    Text("Ambulance: "+name).font(.headline)
+                    Spacer()
+                    if status == "inactive"{
+                        Circle().fill(Color.red).frame(width: 25, height: 25).overlay(Circle().stroke(Color.black, lineWidth: 1)).padding()
+                    }
+                    else if status == "trauma center"{
+                        Circle().fill(Color.green).frame(width: 25, height: 25).overlay(Circle().stroke(Color.black, lineWidth: 1)).padding()
+                    }
+                    else{
+                        Circle().fill(Color.yellow).frame(width: 25, height: 25).overlay(Circle().stroke(Color.black, lineWidth: 1)).padding()
+                    }
+                }.padding(.bottom, -15)
+                
+                if status == "trauma center" {
+                    Divider().background(Color.green)
+                }
+                else if status == "inactive" {
+                    Divider().background(Color.red)
+                }
+                else {
+                    Divider().background(Color.yellow)
+                }
+                
+                Text("En Route To: "+status).font(.headline).padding(.top, 10)
+                Text("Arriving In: "+estimatedArrival).font(.headline)
+            }.padding().padding(.horizontal).cornerRadius(10.0).frame(maxHeight: 300).overlay(RoundedRectangle(cornerRadius: 10.0).stroke(Color.black, lineWidth: 3)).padding().onTapGesture {
+                withAnimation{
+                    viewModel.currentAmbulance = ambulance
+                }
+            }
+        }
     }
 }
 
 struct AmbulanceSelectorView_Previews: PreviewProvider {
     static var previews: some View {
-        AmbulanceSelectorView()
+        AmbulanceSelectorView().environmentObject(MainViewModel())
     }
 }
--- tmobile-5g-hackathon/Models/MainViewModel.swift
+++ tmobile-5g-hackathon/Models/MainViewModel.swift
@@ -7,4 +7,8 @@
 
 import SwiftUI
 
+class MainViewModel: ObservableObject {
+    @Published var currentAmbulance = Ambulance.init(_id: "none", name: "none", going_to: "none", longitude: 50.0, latitude: 51.0, arriving_in: "never", stream_urls: [])
+}
+
 
