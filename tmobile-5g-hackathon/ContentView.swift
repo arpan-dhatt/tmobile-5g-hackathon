ad3f768cb6372dddbb1c758fda6c87c6c64c5e7b
Arpan Dhatt <ary.dhatt@gmail.com>
2021-05-14 20:11:19 -0500

    cleanup before websocket motion jpeg implementation

--- tmobile-5g-hackathon/ContentView.swift
+++ tmobile-5g-hackathon/ContentView.swift
@@ -9,13 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-//        VideoView(videos:[
-//            URL(string: "https://56cdb389b57ec.streamlock.net:1935/nps/faithful.stream/playlist.m3u8")!,
-//            URL(string: "https://56cdb389b57ec.streamlock.net:1935/nps/faithful.stream/playlist.m3u8")!,
-//            URL(string: "https://56cdb389b57ec.streamlock.net:1935/nps/faithful.stream/playlist.m3u8")!,
-//            URL(string: "https://56cdb389b57ec.streamlock.net:1935/nps/faithful.stream/playlist.m3u8")!,
-//        ]).ignoresSafeArea()
-        MicroPhoneButton()
+        VideoView(videos:[
+            URL(string: "https://56cdb389b57ec.streamlock.net:1935/nps/faithful.stream/playlist.m3u8")!,
+            URL(string: "https://56cdb389b57ec.streamlock.net:1935/nps/faithful.stream/playlist.m3u8")!,
+            URL(string: "https://56cdb389b57ec.streamlock.net:1935/nps/faithful.stream/playlist.m3u8")!,
+            URL(string: "https://56cdb389b57ec.streamlock.net:1935/nps/faithful.stream/playlist.m3u8")!,
+        ]).ignoresSafeArea()
     }
 }
 
